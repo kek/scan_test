85e07ce3dc85d774b7c72a3cdd228a2ca9cbcd3b
Karl Eklund <localpart@gmail.com>
2019-08-06 19:11:25 +0200

    Add supervisor

---
+++ lib/scan_test/application.ex
@@ -0,0 +1,21 @@
+defmodule ScanTest.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: ScanTest.Worker.start_link(arg)
+      # {ScanTest.Worker, arg}
+      {Redix, name: :redix}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ScanTest.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
