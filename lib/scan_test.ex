b7ba4b4f292a31412aaee263c3dfb9cbdb2c7980
Karl Eklund <localpart@gmail.com>
2019-08-06 18:51:02 +0200

    new project

---
+++ lib/scan_test.ex
@@ -0,0 +1,18 @@
+defmodule ScanTest do
+  @moduledoc """
+  Documentation for ScanTest.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ScanTest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/scan_test_test.exs
@@ -0,0 +1,8 @@
+defmodule ScanTestTest do
+  use ExUnit.Case
+  doctest ScanTest
+
+  test "greets the world" do
+    assert ScanTest.hello() == :world
+  end
+end
